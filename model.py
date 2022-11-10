5f83dfa8ed71e97fb78e9791b78a8e913266613c
smakh <makhodaie@viratech.ir>
2022-11-10 19:13:09 +0330

    chert

--- model.py
+++ model.py
@@ -99,6 +99,8 @@ class ReallocationModel(TorchModelV2, nn.Module):
         # obs = torch.transpose(obs, 1, 2)
         obs = torch.flatten(obs, 2)
         obs = obs[:,-1,:]
+        print(len(states))
+        print(states[0].shape)
         state = (states[0][0,:,:], states[1][0,:,:])
         print("-----------OBS=----------------\n")
         print(obs.shape)
